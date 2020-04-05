73edf9498ae40f3095894d32d7ec2a93aecab94a
amankr96 <aman.kr96@gmail.com>
2020-04-05 17:46:43 +0530

    Adding some modifiers

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ Vue.directive('highlight',{
   bind(el, binding, vnode){
     // el.style.backgroundColor = 'green';
     //el.style.backgroundColor = binding.value;
-    if(binding.arg == 'background'){
-      el.style.backgroundColor = binding.value;
+    var delay=0;
+    if(binding.modifiers['delayed']) {
+      delay = 3000;
     }
-    else{
-      el.style.color = binding.value;
-    }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      }
+      else{
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 
